d9462ce18c822188541534541aec64c57f639432
Daria Evdok <evda1011@h-ka.de>
2026-05-11 11:01:29 +0200

    Verbesserungen gelegt, das Code Funktioniert

--- hochschule.py
+++ hochschule.py
@@ -1,18 +1,35 @@
-
-
 class Person:
     """ Klasse zur Repräsentation von Personen """
     def __init__(self, name=None, geburtsjahr=None):
         self._name = name
         self._geburtsjahr = geburtsjahr
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def geburtsjahr(self):
+        return self._geburtsjahr
+
+    @geburtsjahr.setter
+    def geburtsjahr(self, value):
+        self._geburtsjahr = value
+
     def __str__(self):
         return f"Person: {self.name}, Geburtsjahr: {self.geburtsjahr}"
 
+    def drucken(self):
+        print(self)
+
 
 class Studierender(Person):
-    """ Klass Studierender als Spezialisierung von Person """
-    def __init__(self, name=None, geburtsjahr=None, matrNR=None):
+    """ Klasse Studierender als Spezialisierung von Person """
+    def __init__(self, name=None, geburtsjahr=None, matrNr=None):
         super().__init__(name=name, geburtsjahr=geburtsjahr)
         self._matrNr = matrNr
 
@@ -30,9 +47,9 @@ class Studierender(Person):
 
 class Dozent(Person):
     """ Klasse Dozent als Spezialisierung von Person """
-    def __init__(self, name, geburtsjahr):
-        super().__init__(name=name, geburtsjahr=geburtsjahr, besGr=None )
-        self._besGr = besGr
+    def __init__(self, name=None, geburtsjahr=None, besoldungsgruppe=None):
+        super().__init__(name=name, geburtsjahr=geburtsjahr)
+        self._besoldungsgruppe = besoldungsgruppe
 
     @property
     def besoldungsgruppe(self):
@@ -45,41 +62,44 @@ class Dozent(Person):
     def __str__(self):
         return f"{super().__str__()} und gleichzeitig Dozent mit Besoldungsgruppe {self.besoldungsgruppe}"
 
-class Angestellter():
-    """ Klasse zur Repräsentation von Angestellten """
-    def __init__(self, vetragsjahr=None):
-        self._vertragsjahr = vetragsjahr
+
+class Angestellter:
+    """  Klasse zur Repräsentation von Angestellten  """
+    def __init__(self, vertragsjahr=None):
+        self._vertragsjahr = vertragsjahr
 
     def verlaengern(self):
-        """ verlängert den Vertag um ein Jahr """
-        self._vertragsjahr += 1
-    
+        """ Продлить контракт на год """
+        if self._vertragsjahr is not None:
+            self._vertragsjahr += 1
+
     def __str__(self):
         return f"Angestellter bis {self._vertragsjahr}"
-    
+
     def drucken(self):
         print(self)
 
+
 class Dozent2(Person, Angestellter):
-    """ Klasse Dozent als Spezialisierung von Person """
+    """ Dozent + Angestellter (множественное наследование) """
     def __init__(self, name=None, geburtsjahr=None, vertragsjahr=None, besoldungsgruppe=None):
         Person.__init__(self, name=name, geburtsjahr=geburtsjahr)
         Angestellter.__init__(self, vertragsjahr=vertragsjahr)
         self._besoldungsgruppe = besoldungsgruppe
+
     def __str__(self):
-        return f"{Person.__str__(self)}, {Angestellter.__str__(self)} " \
-        f"und gleichzeitig Dozent mit Besoldungsgruppe {self._besoldungsgruppe}"
-    
+        return (f"{Person.__str__(self)}, {Angestellter.__str__(self)} "
+                f"und gleichzeitig Dozent mit Besoldungsgruppe {self._besoldungsgruppe}")
 
 
-
-
-# ============= Daten Einfügen =============
+# ============= Тестирование =============
 s2 = Studierender(name="Petra", geburtsjahr=1999, matrNr=123457)
 d1 = Dozent(name="Hans", geburtsjahr=1984, besoldungsgruppe="W2")
 p1 = Person(name="Julia", geburtsjahr=2001)
+
 personen_liste = [s2, p1, d1]
 
 for p in personen_liste:
-    print(type(p))
+    print(type(p).__name__)
     p.drucken()
+    print("-" * 70)
--- hochschule_abstrakt.py
+++ hochschule_abstrakt.py
@@ -1,11 +1,27 @@
 from abc import ABC, abstractmethod
 
 class Person(ABC):
-    """  Abstrakte Klasse zur Repräsentation von Personen.
-Die Methode reset_default() ist von den Subklassen zu implementieren """
+    """ Abstrakte Klasse zur Repräsentation von Personen.
+        Die Methode reset_default() ist von den Subklassen zu implementieren """
     def __init__(self, name=None, geburtsjahr=None):
         self._name = name
         self._geburtsjahr = geburtsjahr
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def geburtsjahr(self):
+        return self._geburtsjahr
+
+    @geburtsjahr.setter
+    def geburtsjahr(self, value):
+        self._geburtsjahr = value
 
     def __str__(self):
         return f"Person: {self.name}, Geburtsjahr: {self.geburtsjahr}"
@@ -16,8 +32,8 @@ Die Methode reset_default() ist von den Subklassen zu implementieren """
 
 
 class Studierender(Person):
-    """ Klass Studierender als Spezialisierung von Person """
-    def __init__(self, name=None, geburtsjahr=None, matrNR=None):
+    """ Klasse Studierender als Spezialisierung von Person """
+    def __init__(self, name=None, geburtsjahr=None, matrNr=None):
         super().__init__(name=name, geburtsjahr=geburtsjahr)
         self._matrNr = matrNr
 
@@ -33,18 +49,16 @@ class Studierender(Person):
         return f"{super().__str__()} und gleichzeitig Studierender mit Matrikelnummer {self.matrNr}"
 
     def reset_default_values(self):
-        """ Zurücksetzen der Attribute auf Standardwerte """
         self.name = "Standard-Studierender"
         self.geburtsjahr = None
         self.matrNr = None
 
 
-
 class Dozent(Person):
     """ Klasse Dozent als Spezialisierung von Person """
-    def __init__(self, name, geburtsjahr):
-        super().__init__(name=name, geburtsjahr=geburtsjahr, besGr=None )
-        self._besGr = besGr
+    def __init__(self, name=None, geburtsjahr=None, besoldungsgruppe=None):
+        super().__init__(name=name, geburtsjahr=geburtsjahr)
+        self._besoldungsgruppe = besoldungsgruppe
 
     @property
     def besoldungsgruppe(self):
@@ -58,46 +72,22 @@ class Dozent(Person):
         return f"{super().__str__()} und gleichzeitig Dozent mit Besoldungsgruppe {self.besoldungsgruppe}"
 
     def reset_default_values(self):
-        """ Zurücksetzen der Attribute auf Standardwerte """
         self.name = "Standard-Dozent"
         self.geburtsjahr = 1900
         self.besoldungsgruppe = None
 
 
-class Angestellter():
-    """ Klasse zur Repräsentation von Angestellten """
-    def __init__(self, vetragsjahr=None):
-        self._vertragsjahr = vetragsjahr
-
-    def verlaengern(self):
-        """ verlängert den Vertag um ein Jahr """
-        self._vertragsjahr += 1
-    
-    def __str__(self):
-        return f"Angestellter bis {self._vertragsjahr}"
-    
-    def drucken(self):
-        print(self)
-
-class Dozent2(Person, Angestellter):
-    """ Klasse Dozent als Spezialisierung von Person """
-    def __init__(self, name=None, geburtsjahr=None, vertragsjahr=None, besoldungsgruppe=None):
-        Person.__init__(self, name=name, geburtsjahr=geburtsjahr)
-        Angestellter.__init__(self, vertragsjahr=vertragsjahr)
-        self._besoldungsgruppe = besoldungsgruppe
-    def __str__(self):
-        return f"{Person.__str__(self)}, {Angestellter.__str__(self)} " \
-        f"und gleichzeitig Dozent mit Besoldungsgruppe {self._besoldungsgruppe}"
-    
+# Добавляем метод drucken ко всем
+Person.drucken = lambda self: print(self)
+Studierender.drucken = lambda self: print(self)
+Dozent.drucken = lambda self: print(self)
 
 
-
-
-# ============= Daten Einfügen =============
+# Тестирование
 s2 = Studierender(name="Petra", geburtsjahr=1999, matrNr=123457)
 d1 = Dozent(name="Hans", geburtsjahr=1984, besoldungsgruppe="W2")
-p1 = Person(name="Julia", geburtsjahr=2001)
-personen_liste = [s2, p1, d1]
+
+personen_liste = [s2, d1]
 
 for p in personen_liste:
     print(type(p))
